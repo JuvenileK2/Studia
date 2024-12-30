4614600a3474e16eb156657c92db533b2dc5343b
JuvenileK2 <kamilzajac@onet.pl>
2024-12-30 23:37:36 +0100

    additional changes

---
+++ ZadanieKubus.py
@@ -0,0 +1,23 @@
+def funkcja(a, b):
+    i = 0
+    while abs(b - a) > 0.05: 
+        i += 1
+        print(f"wykonanie: {i}")
+        print(f"wartosc {round(a, 3)}, {round(b, 3)}") 
+        d = 0.62 * (b - a)  
+        x1 = a + d  
+        x2 = b - d  
+
+        fx1 = x1**3 + (0.5 * x1)**2 - 4 * x1 - 1
+        fx2 = x2**3 + (0.5 * x2)**2 - 4 * x2 - 1
+
+        if fx1 > fx2: 
+            b = x1 
+        elif fx2 > fx1: 
+            a = x2 
+
+    print(f"najmniejsza znaleziona wartość z ostatniej iteracji to punkt: {round(b, 3)}, {round(fx2, 5)}")
+    return b, fx2
+
+Funkcja = funkcja(-0.5, 5.0) 
+print(Funkcja)

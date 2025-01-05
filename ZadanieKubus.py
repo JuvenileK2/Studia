555c67a5326a550a41254ac1167b89f1dafd5435
JuvenileK2 <kamilzajac@onet.pl>
2025-01-05 20:10:09 +0100

    Sprawozdanie poczatek

--- ZadanieKubus.py
+++ ZadanieKubus.py
@@ -1,25 +1,36 @@
 def funkcja(a, b):
     i = 0
-    while abs(b - a) > 0.05: 
+    phi = 0.62  # Stała złotego podziału (zaokrąglona)
+
+    while abs(b - a) > 0.05:  # Warunek zatrzymania
         i += 1
-        print(f"wykonanie: {i}")
+        print(f"wykonanie: {i}\t")
         print(f"wartosc {round(a, 3)}, {round(b, 3)}") 
-        d = 0.62 * (b - a)  
+
+        # Obliczanie punktów złotego podziału
+        d = phi * (b - a)  
         x1 = a + d  
         x2 = b - d  
 
+        # Obliczanie wartości funkcji w punktach x1 i x2
         fx1 = (x1**3) + (0.5 * (x1**2)) - (4 * x1) - 1
         fx2 = (x2**3) + (0.5 * (x2**2)) - (4 * x2) - 1
 
         print(f"Wartosc FX1: {round(fx1, 3)}, FX2: {round(fx2, 3)}") 
 
+        # Aktualizacja przedziału
         if fx1 > fx2: 
-            b = x1 
-        elif fx1 < fx2: 
-            a = x2 
+            b = x1  # Zawężamy przedział od prawej strony
+        else: 
+            a = x2  # Zawężamy przedział od lewej strony
 
-    print(f"najmniejsza znaleziona wartość z ostatniej iteracji to punkt: {round(b, 5)}, {round(fx2, 5)}")
-    return b, fx2
+    # Obliczenie minimum
+    x_min = (a + b) / 2
+    f_min = (x_min**3) + (0.5 * (x_min**2)) - (4 * x_min) - 1
 
-Funkcja = funkcja(-0.5, 5.0) 
+    print(f"najmniejsza znaleziona wartość z ostatniej iteracji to punkt: {round(x_min, 5)}, {round(f_min, 5)}")
+    return x_min
+
+# Wywołanie funkcji
+Funkcja = funkcja(-0.5, 5.0)
 print(Funkcja)
